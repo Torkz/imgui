95678a5798278ec8a927148e68a12f8ccf8239de
Torkz <93553904+Torkz@users.noreply.github.com>
2021-12-29 23:33:36 +0100

    cleaned premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,31 +1,36 @@
 --premake5.lua
 
 project "ImGui"
-    kind "StaticLib"
-    language "C++"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
-    targetdir ("bin/" .. output_dir .. "/%{prj.name}")
-    objdir ("bin-int/" .. output_dir .. "/%{prj.name}")
+	targetdir ("bin/" .. output_dir .. "/%{prj.name}")
+	objdir ("bin-int/" .. output_dir .. "/%{prj.name}")
 
-    files
-    {
-        "imconfig.h",
-        "imgui.cpp",
-        "imgui.h",
-        "imgui_demo.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_tables.cpp",
-        "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h"
-    }
+	files
+	{
+		"imconfig.h",
+		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h"
+	}
+		
+	filter "system:windows"
+		systemversion "latest"
 
-    filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
+	filter "configurations:Debug"
+		symbols "on"
+		runtime "Debug"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	filter "configurations:Release"
+		optimize "on"
+		runtime "Release"
